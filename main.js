03c1bf2163620bcecf01b96155398ee663fc7a0b
nallsaa <89607954+nallsaa@users.noreply.github.com>
2021-11-12 17:49:14 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var first_name="Saanvi";
+var last_name="Nalla";
+full_name=first_name.concat(last_name)
+console.log(full_name)
+var a = 10;
+var b = 5;
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
